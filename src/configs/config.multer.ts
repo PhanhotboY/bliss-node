c42ad2224727eb5b886ec1e73088e2712bd06295
Phan <nguyenduyphan2003@gmail.com>
2025-03-19 12:45:36 +0700

    fix

--- src/configs/config.multer.ts
+++ src/configs/config.multer.ts
@@ -8,7 +8,16 @@ const UPLOAD_SIZE_LIMIT = 5 * 1024 * 1024;
 
 const diskStorage = multer({
   storage: multer.diskStorage({
-    destination: UPLOAD_FOLDER,
+    destination: (req, file, cb) => {
+      try {
+        if (!fs.existsSync(UPLOAD_FOLDER)) {
+          fs.mkdirSync(UPLOAD_FOLDER);
+        }
+        cb(null, UPLOAD_FOLDER);
+      } catch (error) {
+        console.error(error);
+      }
+    },
     filename: (req, file, cb) => {
       const decodedFilename = Buffer.from(file.originalname, 'latin1').toString(
         'utf8'
